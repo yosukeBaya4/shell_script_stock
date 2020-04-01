7f4ec4d01d64d064b5a103d137ae84c626062b63
yosukeBaya4 <yousken_n72@msn.com>
2020-04-01 23:12:51 +0900

    changed a bit.

---
+++ test_prime.py
@@ -0,0 +1,27 @@
+from prime import is_prime
+
+"""
+def test_is_prime():
+    assert not is_prime(1)
+    assert is_prime(2)
+    assert is_prime(3)
+    assert not is_prime(4)
+    assert is_prime(5)
+    assert not is_prime(6)
+    assert is_prime(7)
+    assert not is_prime(10)
+    assert not is_prime(8)
+    assert not is_prime(9)
+"""
+
+def test_is_prime():
+    assert is_prime(1) == False
+    assert is_prime(2) == True
+    assert is_prime(3) == True
+    assert is_prime(4) == False
+    assert is_prime(5) == True
+    assert is_prime(6) == False
+    assert is_prime(7) == True
+    assert is_prime(8) == False
+    assert is_prime(9) == False
+    assert is_prime(10) == False
